d65afd19d780e1f69425865b361f128a1ed49056
mfuentealba <mario.fuentealba.otarola@gmail.com>
2016-10-14 01:16:40 -0300

    manejo de zip

--- descarga.php
+++ descarga.php
@@ -14,8 +14,8 @@ $objMes->{"10"} = "octubre";
 $objMes->{"11"} = "noviembre";
 $objMes->{"12"} = "diciembre";
 
-
-for($x = 2015; $x < 2017; $x++){
+/*
+for($x = 2004; $x < 2017; $x++){
 	$url = $urlBase . $x;
 	$contenido = file_get_contents($url);
 	//echo "El contenido de la URL es:n";
@@ -43,12 +43,88 @@ for($x = 2015; $x < 2017; $x++){
 			$arrZIP[0][$j] = $arr[0][$i] . '/' . $arrZIP[0][$j];
 			$contenido = file_get_contents($arrZIP[0][$j]);
 			//echo $contenido;
-			file_put_contents ("EURUSD\\EURUSD" . $x . "\\" . $mes . "\\" . $nomFile, $contenido);			
+			file_put_contents ("EURUSD\\EURUSD" . $x . "_" . $mes . "_" . $nomFile, $contenido);			
 			//exit();
 		}
 	}
 	
 
+}*/
+
+/*
+	LOAD DATA INFILE 'c:/tmp/discounts_2.csv'
+INTO TABLE discounts
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(title,@expired_date,amount)
+SET expired_date = STR_TO_DATE(@expired_date, '%m/%d/%Y');
+	*/
+$zip = new ZipArchive;
+if($zip->open('EURUSD\\EURUSD2009_noviembre_EUR_USD_Week4.zip') === TRUE) {
+    $zip->extractTo('EURUSD\\Procesados');
+    $zip->close();
+    echo 'ok';
+} else {
+    echo 'failed';
+}
+
+$username = "forex";
+$password = "forex";
+$server = "localhost";
+$port = "3306";
+$databasename = "forex";
+$tablename = "EURUSD";	
+$connection = mysqli_connect(
+						$server,  
+						$username,  
+						$password, 
+						$databasename,
+						$port
+					);
+
+/*$stmt = mysqli_ prepare($connection, "LOAD DATA INFILE 'EURUSD\\Procesados\\EUR_USD_Week4.csv'
+INTO TABLE eurusd
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '\"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;");
+*/
+$str = "LOAD DATA INFILE 'C:\\\\Users\\\\mario\\\\EURUSD\\\\Procesados\\\\EUR_USD_Week4.csv'
+INTO TABLE eurusd
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '\"'
+LINES TERMINATED BY '\\n'
+IGNORE 1 ROWS;";
+echo $str;
+if ($connection->query($str) === TRUE) {
+    printf("Se creó con éxtio la tabla myCity.\n");
+} else {
+	printf("MAL.\n");
+}		
+
+/*$msg = throwExceptionOnError($connection);
+if($msg != ''){
+	echo $msg;
+	exit();
+}
+
+mysqli_stmt_execute($stmt);
+
+$msg = throwExceptionOnError($connection);
+if($msg != ''){
+	echo $msg;
+	exit();
+}
+*/
+function throwExceptionOnError($link = null) {
 	
-	
+	if(mysqli_error($link)) {
+		$msg = mysqli_errno($link) . ": " . mysqli_error($link);
+		return 'MySQL Error - '. $msg;
+		throw new Exception('MySQL Error - '. $msg);
+		
+	} else {
+		return '';
+	}		
 }
